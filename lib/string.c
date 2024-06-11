8d007213e10eaebe19bd1c66f4b6ca7ab825ed4a
yiezias <yiezias@outlook.com>
2024-06-11 16:19:07 +0800

    string.c添加断言

--- lib/string.c
+++ lib/string.c
@@ -1,19 +1,23 @@
 
 #include "string.h"
+#include "debug.h"
 
 void memcpy(void *dst, const void *src, size_t size) {
+	ASSERT(dst && src);
 	while (size--) {
 		*(char *)(dst++) = *(char *)(src++);
 	}
 }
 
 void memset(void *dst, uint8_t value, size_t size) {
+	ASSERT(dst);
 	while (size--) {
 		*(uint8_t *)dst++ = value;
 	}
 }
 
 char *strcpy(char *dst, const char *src) {
+	ASSERT(dst && src);
 	char *ret = dst;
 	while ((*dst++ = *src++)) {}
 	return ret;
