6146240dd3ec4272250fee5873fdf7140209e5a0
yiezias <yiezias@outlook.com>
2024-06-11 15:51:52 +0800

    定义段选择子（根据之前的gdt）

---
+++ kernel/global.h
@@ -0,0 +1,9 @@
+#ifndef __KERNEL_GLOBAL_H
+#define __KERNEL_GLOBAL_H
+
+#define SELECTOR_K_CODE 8
+#define SELECTOR_K_DATA 0x10
+#define SELECTOR_U_CODE (0x30 + 3)
+#define SELECTOR_U_DATA (0x28 + 3)
+
+#endif
