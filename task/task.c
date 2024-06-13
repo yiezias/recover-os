59127bf59c54e1f8e1a28d5c5fbfb1aaf7f592f3
yiezias <yiezias@outlook.com>
2024-06-13 13:27:36 +0800

    添加task_struct

--- task/task.c
+++ task/task.c
@@ -1,8 +1,20 @@
 #include "task.h"
 #include "print.h"
+#include "tss.h"
 
+
+static void make_main_task(void) {
+	struct task_struct *main_task;
+	asm volatile("movq %%rsp,%0;"
+		     "andq $0xfffffffffffff000,%0"
+		     : "=g"(main_task));
+	put_info("main_task: ", (size_t)main_task);
+}
 
 void task_init(void) {
 	put_str("task_init: start\n");
+
+	make_main_task();
+
 	put_str("task_init: end\n");
 }
--- task/task.h
+++ task/task.h
@@ -1,6 +1,11 @@
 #ifndef __TASK_TASK_H
 #define __TASK_TASK_H
+#include "types.h"
 
 void task_init(void);
+
+struct task_struct {
+	uint64_t stack_magic;
+};
 
 #endif
