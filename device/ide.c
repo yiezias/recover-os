d0d2edd5568bd8651e6939d9c8ff5800d5c20ebb
yiezias <yiezias@outlook.com>
2024-06-15 16:17:17 +0800

    添加ide.*

---
+++ device/ide.c
@@ -0,0 +1,8 @@
+#include "ide.h"
+#include "print.h"
+
+void ide_init(void) {
+	put_str("ide_init: start\n");
+
+	put_str("ide_init: end\n");
+}
---
+++ device/ide.h
@@ -0,0 +1,6 @@
+#ifndef __DEVICE_IDE_H
+#define __DEVICE_IDE_H
+
+void ide_init(void);
+
+#endif
