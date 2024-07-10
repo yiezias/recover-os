b100de048f8978962b6fd23d074902a9ccdd870c
yiezias <yiezias@outlook.com>
2024-07-10 17:14:40 +0800

    添加超级块和格式化

--- fs/fs.c
+++ fs/fs.c
@@ -1,6 +1,8 @@
 #include "fs.h"
+#include "bio.h"
 #include "debug.h"
 #include "ide.h"
+#include "memory.h"
 #include "print.h"
 
 struct partition_table_entry {
@@ -38,10 +40,25 @@ static void mbr_init(void) {
 	PANIC("There isn't any bootable partition\n");
 }
 
+
+struct super_block *sb;
+static void partition_format(void) {
+	//	sb->magic = SB_MAGIC;
+	size_t super_block_bid = cur_part->start_lba / SECTS_PER_BLOCK;
+	size_t block_cnt = cur_part->start_sec / SECTS_PER_BLOCK;
+	sb->block_bitmap_start = super_block_bid + 1;
+	sb->block_bitmap_size = block_cnt;
+}
 void filesys_init(void) {
 	put_str("filesys_init: start\n");
 
 	mbr_init();
+
+	sb = alloc_pages(1);
+	block_read(0, cur_part->start_lba / SECTS_PER_BLOCK, sb, BLOCK_SIZE, 0);
+	if (sb->magic != SB_MAGIC) {
+		partition_format();
+	}
 
 	put_str("filesys_init: end\n");
 }
--- fs/fs.h
+++ fs/fs.h
@@ -1,5 +1,17 @@
 #ifndef __FS_FS_H
 #define __FS_FS_H
+#include "types.h"
+
+#define SB_MAGIC 0x474d575a5a46594c
+struct super_block {
+	size_t magic;
+	size_t block_bitmap_start;
+	size_t block_bitmap_size;
+	size_t inode_bitmap_start;
+	size_t inode_table_start;
+	size_t inode_cnt_max;
+	size_t data_start;
+};
 
 void filesys_init(void);
 
